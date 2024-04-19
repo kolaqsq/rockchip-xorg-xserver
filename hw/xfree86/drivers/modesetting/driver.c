JeffyCN/xorg-xserver

modesetting: Support WarmUp option
Support warming up by ignoring the first display request.

This option is enabled by default.

Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

hw/xfree86/drivers/modesetting/driver.c
@@ -149,6 +149,7 @@ static const OptionInfoRec Options[] = {
     {OPTION_FLIP_FB_RATE, "MaxFlipRate", OPTV_INTEGER, {0}, 0},
     {OPTION_NO_EDID, "NoEDID", OPTV_BOOLEAN, {0}, FALSE},
     {OPTION_HOTPLUG_RESET, "HotplugReset", OPTV_BOOLEAN, {0}, TRUE},
+    {OPTION_WARM_UP, "WarmUp", OPTV_BOOLEAN, {0}, TRUE},
     {-1, NULL, OPTV_NONE, {0}, FALSE}
 };
 
@@ -748,6 +749,12 @@ msBlockHandler_oneshot(ScreenPtr pScreen, void *pTimeout)
     ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
     modesettingPtr ms = modesettingPTR(pScrn);
 
+    /* HACK: Ignore the first request */
+    if (ms->warm_up) {
+        ms->warm_up = FALSE;
+        return;
+    }
+
     msBlockHandler(pScreen, pTimeout);
 
     drmmode_set_desired_modes(pScrn, &ms->drmmode, TRUE);
@@ -1089,6 +1096,9 @@ PreInit(ScrnInfoPtr pScrn, int flags)
 
     ms->drmmode.hotplug_reset =
         xf86ReturnOptValBool(ms->drmmode.Options, OPTION_HOTPLUG_RESET, TRUE);
+
+    ms->warm_up =
+        xf86ReturnOptValBool(ms->drmmode.Options, OPTION_WARM_UP, TRUE);
 
     str_value = xf86GetOptValString(ms->drmmode.Options, OPTION_FLIP_FB);
     if (!str_value || !strcmp(str_value, "transformed"))

kolaqsq/rockchip-xorg-xserver

hw/xfree86/drivers/modesetting/driver.c
@@ -147,6 +147,7 @@ static const OptionInfoRec Options[] = {
     {OPTION_BIND_CURRENT, "BindCurrent", OPTV_BOOLEAN, {0}, FALSE},
     {OPTION_NO_EDID, "NoEDID", OPTV_BOOLEAN, {0}, FALSE},
     {OPTION_HOTPLUG_RESET, "HotplugReset", OPTV_BOOLEAN, {0}, FALSE},
+    {OPTION_WARM_UP, "WarmUp", OPTV_BOOLEAN, {0}, FALSE},
     {-1, NULL, OPTV_NONE, {0}, FALSE}
 };
 
@@ -728,6 +729,12 @@ msBlockHandler_oneshot(ScreenPtr pScreen, void *pTimeout)
     ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
     modesettingPtr ms = modesettingPTR(pScrn);
 
+    /* HACK: Ignore the first request */
+    if (ms->warm_up) {
+        ms->warm_up = FALSE;
+        return;
+    }
+
     msBlockHandler(pScreen, pTimeout);
 
     drmmode_set_desired_modes(pScrn, &ms->drmmode, TRUE);
@@ -1067,6 +1074,9 @@ PreInit(ScrnInfoPtr pScrn, int flags)
 
     ms->drmmode.hotplug_reset =
         xf86ReturnOptValBool(ms->drmmode.Options, OPTION_HOTPLUG_RESET, TRUE);
+
+    ms->warm_up =
+        xf86ReturnOptValBool(ms->drmmode.Options, OPTION_WARM_UP, TRUE);
 
     str_value = xf86GetOptValString(ms->drmmode.Options, OPTION_FLIP_FB);
     if (!str_value || !strcmp(str_value, "transformed"))